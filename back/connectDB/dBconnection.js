725b55186e36790a42357ba6212597e2cf24c286
MandiSR <mandi.sanchez.97@gmail.com>
2024-12-12 11:54:38 -0300

    Ignorar archivo .env

--- back/connectDB/dBconnection.js
+++ back/connectDB/dBconnection.js
@@ -1,21 +1,49 @@
-const mysql= require('mysql2')
+const mysql = require('mysql2');
 
-const dbConfig={
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'amanda',
-    database: 'tp-final-uade'
+// Obtener la URL de conexión desde la variable de entorno
+const dbUrl = process.env.MYSQL_URL;
+
+// Si la URL de la base de datos no está definida, lanzamos un error.
+if (!dbUrl) {
+    console.error('La variable de entorno MYSQL_URL no está definida.');
+    process.exit(1);
 }
 
-const connection =mysql.createConnection(dbConfig)
+// Crear la conexión utilizando la URL proporcionada por la variable de entorno
+const connection = mysql.createConnection(dbUrl);
 
-connection.connect((err)=>{
-    if(err){
-        console.log("Failed to connect")
-        return
+connection.connect((err) => {
+    if (err) {
+        console.log("Failed to connect", err);
+        return;
     }
-    console.log("Connection established")
-})
+    console.log("Connection established");
+});
 
-module.exports =connection
+module.exports = connection;
+
+
+
+
+
+// const mysql= require('mysql2')
+
+// const dbConfig={
+//     host: 'localhost',
+//     port: '3306',
+//     user: 'root',
+//     password: 'amanda',
+//     database: 'tp-final-uade'
+// }
+
+// const connection =mysql.createConnection(dbConfig)
+
+// connection.connect((err)=>{
+//     if(err){
+//         console.log("Failed to connect")
+//         return
+//     }
+//     console.log("Connection established")
+// })
+
+// module.exports =connection
