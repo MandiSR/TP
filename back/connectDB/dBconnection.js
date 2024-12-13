20c59065bae6daab0d4107571ae5e47b02edabaa
MandiSR <mandi.sanchez.97@gmail.com>
2024-12-13 00:49:10 -0300

    probando deploy

--- back/connectDB/dBconnection.js
+++ back/connectDB/dBconnection.js
@@ -1,3 +1,24 @@
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASSWORD,
+//   database: process.env.MYSQLDATABASE,
+//   port: process.env.MYSQLPORT || 3306,
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Failed to connect to the database:', err.message);
+//     process.exit(1);
+//   }
+//   console.log('Connection to the database established');
+// });
+
+// module.exports = connection;
+
+
 const mysql = require('mysql2');
 
 
